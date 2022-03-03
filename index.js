ba1139c5f48c4fcb25b76b00254cf6683d0ae802
Dennis Perepech <perepech@usc.edu>
2022-03-03 12:25:07 -0800

    t

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+const express = require('express');
+const product = require("./api/product");
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+const app = express();
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+const PORT = process.env.PORT || 5050;
 
-module.exports = app;
+app.use("/api/product", product);
+
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
