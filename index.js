0b6bccc9d5a5e2e3ec3ef7008590cfca4fc91b17
Dennis Perepech <perepech@usc.edu>
2022-03-03 12:41:20 -0800

    a

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
+const product = require("./api/product");
+const PORT = process.env.PORT || 8080;
+
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.use(express.json({extended: false}));
 
-app.get('/about', (req, res) => res.send('About Page Route'));
+app.use("/api/product", product);
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
