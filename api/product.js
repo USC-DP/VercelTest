7f1478bcc4a19389d4e479d1a186dfa5e7546b00
Dennis Perepech <perepech@usc.edu>
2022-03-03 12:36:55 -0800

    z

--- api/product.js
+++ api/product.js
@@ -8,10 +8,15 @@ const router = express.Router();
  */
 
 router.get("/", async (req, res) => {
+    try {
         res.json({
             status: 200,
             message: "Get data has successfully",
         });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
 });
 
 module.exports = router;
